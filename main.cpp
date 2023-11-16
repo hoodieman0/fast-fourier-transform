4c60839f291c99fd1d5e5ebc5c56b0e15abcd1aa
James Mok <hoodieman8@gmail.com>
2023-11-16 02:51:36 -0500

    feat: added test case for testing FFT algorithm

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
+#include "fft.hpp"
+#include <iostream>
+
 int main(){
+    vector<complex<double>> input;
+    input.push_back(complex<double>(1.0, 0.0));
+    input.push_back(complex<double>(2.0, -1.0));
+    input.push_back(complex<double>(0.0, -1.0));
+    input.push_back(complex<double>(-1.0, 2.0));
+
+    vector<complex<double>> output = FourierTransform(input, input.size(), 1);
+    
+    // 2, -2-2i, -2i, 4+4i
+    /*
+        2, 0
+        -2, -2
+        0, -2
+        4, 4
+
+    */
+    for(complex<double> o : output){
+        cout << o << endl;
+    }
+
     return 0;
 }
+
+// go through example
