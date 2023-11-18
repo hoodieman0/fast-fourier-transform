bd92d2b72066a400ce2f042ea35d53a92d5ba06f
James Mok <hoodieman8@gmail.com>
2023-11-18 00:07:59 -0500

    feat: added comments for readability

--- fft.cpp
+++ fft.cpp
@@ -1,23 +1,27 @@
 #include "fft.hpp"
 
-
+// Must be a power of 2
+// Not tested with any other size
 vector<complex<double>> FourierTransform(const vector<complex<double>> input) {
     const int N = input.size();
+    
+    // recursion base case, no work is done
     if (N == 1){
         return input;
     }
 
+    // make the even-indexed and odd-indexed arrays
     vector<complex<double>> transformEven, transformOdd;
-
     for(int i = 0; i < N; i=i+2){
         transformEven.push_back(input[i]);
         if (i+1 > N) break;
         transformOdd.push_back(input[i+1]);
     }
-
+    
     transformEven = FourierTransform(transformEven);
     transformOdd = FourierTransform(transformOdd);
 
+    // make an array of input.size() and fill it with new Fast Fourier Transform values
     vector<complex<double>> output(N);
     complex<double> p, q;
     for (int k = 0; k < N/2; k++){
--- fft.hpp
+++ fft.hpp
@@ -7,13 +7,15 @@
 
 using namespace std;
 
-// complex number of only i
+// complex number i
 #define C_I complex<double>(0.0, 1.0)
 
-// value of PI for doubles and complex numbers
+// PI for doubles and complex numbers
 #define C_PI atan(1) * 4
 
-vector<complex<double>> FourierTransform(const vector<complex<double>> input, const int N, const int s);
+/// @brief A Cooley-Tukey Fast Fourier Transform Algorithm for complex numbers of time nlgn
+/// @param input a std::vector of std::complex<double>
+/// @return a std::vector<complex<double>> of the new transform, the same size as input 
 vector<complex<double>> FourierTransform(const vector<complex<double>> input);
 
 #endif
