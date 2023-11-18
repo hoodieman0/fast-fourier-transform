91d2891495db75ded461d3fda4d2c1e9080d1fb1
James Mok <hoodieman8@gmail.com>
2023-11-17 23:56:40 -0500

    feat: updated FourierTransform to do proper calculations

--- fft.cpp
+++ fft.cpp
@@ -1,29 +1,32 @@
 #include "fft.hpp"
 
-// N must be a multiple of 2
-vector<complex<double>> FourierTransform(const vector<complex<double>> input, const int N, const int s) {
-    vector<complex<double>> transform;
 
+vector<complex<double>> FourierTransform(const vector<complex<double>> input) {
+    const int N = input.size();
     if (N == 1){
-        transform.push_back(input[0]);
-        return transform;
+        return input;
     }
 
-    vector<complex<double>> trans1 = FourierTransform(input, N/2, 2 * s);
-    vector<complex<double>> trans2 = FourierTransform(vector<complex<double>>(input.begin()+s, input.end()), N/2, 2 * s);
+    vector<complex<double>> transformEven, transformOdd;
 
-    // make the list of the transforms into one
-    transform = trans1;
-    transform.insert(transform.end(), trans2.begin(), trans2.end());
+    for(int i = 0; i < N; i=i+2){
+        transformEven.push_back(input[i]);
+        if (i+1 > N) break;
+        transformOdd.push_back(input[i+1]);
+    }
 
+    transformEven = FourierTransform(transformEven);
+    transformOdd = FourierTransform(transformOdd);
 
+    vector<complex<double>> output(N);
     complex<double> p, q;
-    for (int k = 0; k < N/2 - 1; k++){
-        p = transform[k];
-        q = exp((-2 * C_PI * C_I * (double)k)/(double)N) * transform[k+(N/2)];
-        transform[k] = p + q;
-        transform[k+(N/2)] = p - q;
+    for (int k = 0; k < N/2; k++){
+        p = transformEven[k];
+        q = exp((-2 * C_PI * C_I * (double)k)/(double)N) * transformOdd[k];
+        
+        output[k] = p + q;
+        output[k+(N/2)] = p - q;
     }
 
-    return transform;
+    return output;
 }
--- fft.hpp
+++ fft.hpp
@@ -7,9 +7,13 @@
 
 using namespace std;
 
+// complex number of only i
 #define C_I complex<double>(0.0, 1.0)
+
+// value of PI for doubles and complex numbers
 #define C_PI atan(1) * 4
 
 vector<complex<double>> FourierTransform(const vector<complex<double>> input, const int N, const int s);
+vector<complex<double>> FourierTransform(const vector<complex<double>> input);
 
 #endif
